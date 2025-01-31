4d53220ff2cc8fdbac64a9b0902a32e155f1b8cb
AltPath3310 <argx777@gmail.com>
2025-01-31 22:54:56 +0400

    Hide several ui that provide other ways to enter shorts

--- inject_scripts/styles_inject.js
+++ inject_scripts/styles_inject.js
@@ -3,8 +3,20 @@ function injectCSS() {
 		const style = document.createElement("style");
 		style.id = "injected-css";
 		style.textContent = `
-		.style-scope .ytd-rich-shelf-renderer {
+		.ytd-rich-shelf-renderer {
 			display: none !important;
+		}
+		
+		ytd-reel-shelf-renderer {
+			display: none !important;
+		}
+		
+		[chip-shape-data*='"text":"Shorts"'] {
+			display: none !important;
+		}
+		
+		.ytd-item-section-renderer[is-search] {
+			display: none !important
 		}
 
 		[title="Shorts"] {
